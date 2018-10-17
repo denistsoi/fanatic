c758bf02090750acde6e88fb9f573f8ab52c82f9
Denis Tsoi <denistsoi@gmail.com>
2018-10-17 18:26:36 +0800

    ADD: heroku sandbox issue;

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fanatic = require('./fanatic');
 
 (async () => {
   console.log("Start of program --- ", new Date())
-  await fanatic({ headless: true });
+  await fanatic({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   console.log("end program")
 })();
